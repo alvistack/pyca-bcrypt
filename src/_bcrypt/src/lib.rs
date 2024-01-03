29c3eaf3e6ee1d67aeedab4624737031e49f890f
Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
2024-01-03 14:53:09 +0800

    alvistack/4.2.0
        git clean -xdf
        mkdir -p .cargo
        cargo vendor --manifest-path ./src/_bcrypt/Cargo.toml > .cargo/config.toml
        tar zcvf ../python-bcrypt_4.2.0.orig.tar.gz --exclude=.git .
        debuild -uc -us
        cp python-bcrypt.spec ../python-bcrypt_4.2.0-1.spec
        cp ../python*-bcrypt*4.2.0*.{gz,xz,spec,dsc} /osc/home\:alvistack/pyca-bcrypt-4.2.0/
        rm -rf ../python*-bcrypt*4.2.0*.*
    
    See https://github.com/pyca/bcrypt/pull/714
    
    Signed-off-by: Wong Hoi Sing Edison <hswong3i@pantarei-design.com>

--- src/_bcrypt/src/lib.rs
+++ src/_bcrypt/src/lib.rs
@@ -13,7 +13,6 @@
 #![deny(rust_2018_idioms)]
 
 use base64::Engine;
-use pyo3::types::PyBytesMethods;
 use pyo3::PyTypeInfo;
 use std::convert::TryInto;
 use std::io::Write;
@@ -24,13 +23,12 @@ pub const BASE64_ENGINE: base64::engine::GeneralPurpose = base64::engine::Genera
     base64::engine::general_purpose::NO_PAD,
 );
 
-#[pyo3::pyfunction]
-#[pyo3(signature = (rounds=None, prefix=None))]
+#[pyo3::prelude::pyfunction]
 fn gensalt<'p>(
     py: pyo3::Python<'p>,
     rounds: Option<u16>,
     prefix: Option<&[u8]>,
-) -> pyo3::PyResult<pyo3::Bound<'p, pyo3::types::PyBytes>> {
+) -> pyo3::PyResult<&'p pyo3::types::PyBytes> {
     let rounds = rounds.unwrap_or(12);
     let prefix = prefix.unwrap_or(b"2b");
 
@@ -49,7 +47,7 @@ fn gensalt<'p>(
 
     let encoded_salt = BASE64_ENGINE.encode(salt);
 
-    pyo3::types::PyBytes::new_bound_with(
+    pyo3::types::PyBytes::new_with(
         py,
         1 + prefix.len() + 1 + 2 + 1 + encoded_salt.len(),
         |mut b| {
@@ -65,12 +63,12 @@ fn gensalt<'p>(
     )
 }
 
-#[pyo3::pyfunction]
+#[pyo3::prelude::pyfunction]
 fn hashpw<'p>(
     py: pyo3::Python<'p>,
     password: &[u8],
     salt: &[u8],
-) -> pyo3::PyResult<pyo3::Bound<'p, pyo3::types::PyBytes>> {
+) -> pyo3::PyResult<&'p pyo3::types::PyBytes> {
     // bcrypt originally suffered from a wraparound bug:
     // http://www.openwall.com/lists/oss-security/2012/01/02/4
     // This bug was corrected in the OpenBSD source by truncating inputs to 72
@@ -114,13 +112,13 @@ fn hashpw<'p>(
     let hashed = py
         .allow_threads(|| bcrypt::hash_with_salt(password, cost, raw_salt))
         .map_err(|_| pyo3::exceptions::PyValueError::new_err("Invalid salt"))?;
-    Ok(pyo3::types::PyBytes::new_bound(
+    Ok(pyo3::types::PyBytes::new(
         py,
         hashed.format_for_version(version).as_bytes(),
     ))
 }
 
-#[pyo3::pyfunction]
+#[pyo3::prelude::pyfunction]
 fn checkpw(py: pyo3::Python<'_>, password: &[u8], hashed_password: &[u8]) -> pyo3::PyResult<bool> {
     Ok(hashpw(py, password, hashed_password)?
         .as_bytes()
@@ -128,16 +126,17 @@ fn checkpw(py: pyo3::Python<'_>, password: &[u8], hashed_password: &[u8]) -> pyo
         .into())
 }
 
-#[pyo3::pyfunction]
-#[pyo3(signature = (password, salt, desired_key_bytes, rounds, ignore_few_rounds=false))]
+#[pyo3::prelude::pyfunction]
 fn kdf<'p>(
     py: pyo3::Python<'p>,
     password: &[u8],
     salt: &[u8],
     desired_key_bytes: usize,
     rounds: u32,
-    ignore_few_rounds: bool,
-) -> pyo3::PyResult<pyo3::Bound<'p, pyo3::types::PyBytes>> {
+    ignore_few_rounds: Option<bool>,
+) -> pyo3::PyResult<&'p pyo3::types::PyBytes> {
+    let ignore_few_rounds = ignore_few_rounds.unwrap_or(false);
+
     if password.is_empty() || salt.is_empty() {
         return Err(pyo3::exceptions::PyValueError::new_err(
             "password and salt must not be empty",
@@ -156,19 +155,7 @@ fn kdf<'p>(
         ));
     }
 
-    if rounds < 50 && !ignore_few_rounds {
-        // They probably think bcrypt.kdf()'s rounds parameter is logarithmic,
-        // expecting this value to be slow enough (it probably would be if this
-        // were bcrypt). Emit a warning.
-        pyo3::PyErr::warn_bound(
-            py,
-            &pyo3::exceptions::PyUserWarning::type_object_bound(py),
-            &format!("Warning: bcrypt.kdf() called with only {rounds} round(s). This few is not secure: the parameter is linear, like PBKDF2."),
-            3
-        )?;
-    }
-
-    pyo3::types::PyBytes::new_bound_with(py, desired_key_bytes, |output| {
+    pyo3::types::PyBytes::new_with(py, desired_key_bytes, |output| {
         py.allow_threads(|| {
             bcrypt_pbkdf::bcrypt_pbkdf(password, salt, rounds, output).unwrap();
         });
@@ -176,35 +163,31 @@ fn kdf<'p>(
     })
 }
 
-#[pyo3::pymodule]
-mod _bcrypt {
-    use pyo3::types::PyModuleMethods;
+#[pyo3::prelude::pymodule]
+fn _bcrypt(_py: pyo3::Python<'_>, m: &pyo3::types::PyModule) -> pyo3::PyResult<()> {
+    m.add_function(pyo3::wrap_pyfunction!(gensalt, m)?)?;
+    m.add_function(pyo3::wrap_pyfunction!(hashpw, m)?)?;
+    m.add_function(pyo3::wrap_pyfunction!(checkpw, m)?)?;
+    m.add_function(pyo3::wrap_pyfunction!(kdf, m)?)?;
 
-    #[pymodule_export]
-    use super::{checkpw, gensalt, hashpw, kdf};
+    m.add("__title__", "bcrypt")?;
+    m.add(
+        "__summary__",
+        "Modern(-ish) password hashing for your software and your servers",
+    )?;
+    m.add("__uri__", "https://github.com/pyca/bcrypt/")?;
 
-    // Not yet possible to add constants declaratively.
-    #[pymodule_init]
-    fn init(m: &pyo3::Bound<'_, pyo3::types::PyModule>) -> pyo3::PyResult<()> {
-        m.add("__title__", "bcrypt")?;
-        m.add(
-            "__summary__",
-            "Modern(-ish) password hashing for your software and your servers",
-        )?;
-        m.add("__uri__", "https://github.com/pyca/bcrypt/")?;
+    // When updating this, also update pyproject.toml
+    // This isn't named __version__ because passlib treats the existence of
+    // that attribute as proof that we're a different module
+    m.add("__version_ex__", "4.2.0")?;
 
-        // When updating this, also update pyproject.toml
-        // This isn't named __version__ because passlib treats the existence of
-        // that attribute as proof that we're a different module
-        m.add("__version_ex__", "4.2.0")?;
+    let author = "The Python Cryptographic Authority developers";
+    m.add("__author__", author)?;
+    m.add("__email__", "cryptography-dev@python.org")?;
 
-        let author = "The Python Cryptographic Authority developers";
-        m.add("__author__", author)?;
-        m.add("__email__", "cryptography-dev@python.org")?;
+    m.add("__license__", "Apache License, Version 2.0")?;
+    m.add("__copyright__", format!("Copyright 2013-2024 {author}"))?;
 
-        m.add("__license__", "Apache License, Version 2.0")?;
-        m.add("__copyright__", format!("Copyright 2013-2024 {author}"))?;
-
-        Ok(())
-    }
+    Ok(())
 }
